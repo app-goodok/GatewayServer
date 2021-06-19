a85ca408ba179be1a6b02ebf2c1906392dc54b52
Konstantin Morozov <morozov-kst@yandex.ru>
2021-06-19 22:47:49 +0300

    update dockers

--- env/server/script.sh
+++ env/server/script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker pull docker.pkg.github.com/app-goodok/gatewayserver/common-env:0.1.1
-docker build -t docker.pkg.github.com/app-goodok/gatewayserver/gateway-server:0.1.1 .
-docker push docker.pkg.github.com/app-goodok/gatewayserver/gateway-server:0.1.1
+docker build -t ghcr.io/app-goodok/gatewayserver/common-env:0.1.1 .
+docker push ghcr.io/app-goodok/gatewayserver/common-env:0.1.1
